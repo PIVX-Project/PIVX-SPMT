8529da6849fb2b5a2496061934ed6dccd742fa4c
gpdionisio <gianpiero.dionisio@protonmail.ch>
2018-06-26 11:58:32 +0200

    Proposal details Dialog: make hashes and address selectable

--- src/qt/dlg_proposalDetails.py
+++ src/qt/dlg_proposalDetails.py
@@ -3,7 +3,7 @@
 import sys
 import os.path
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
-from PyQt5.QtWidgets import QDialog, QFormLayout, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QDialog, QFormLayout, QVBoxLayout, QLabel, QLineEdit
 from PyQt5.Qt import QPushButton
 from PyQt5.QtCore import Qt
 
@@ -37,13 +37,22 @@ class Ui_proposalDetailsDlg(object):
         body.addRow(QLabel("<b>URL: </b>"), link_label)
         body.addRow(QLabel("<b>TotalPayment: </b>"), QLabel(str(PropDetailsDlg.data.ToalPayment)))
         body.addRow(QLabel("<b>MonthlyPayment: </b>"), QLabel(str(PropDetailsDlg.data.MonthlyPayment)))
-        body.addRow(QLabel("<b>Hash: </b>"), QLabel(PropDetailsDlg.data.Hash))
-        body.addRow(QLabel("<b>FeeHash: </b>"), QLabel(PropDetailsDlg.data.FeeHash))
+        hashLabel = QLineEdit(PropDetailsDlg.data.Hash)
+        hashLabel.setMinimumWidth(420)
+        hashLabel.setReadOnly(True)
+        body.addRow(QLabel("<b>Hash: </b>"), hashLabel)
+        feeHashLabel = QLineEdit(PropDetailsDlg.data.FeeHash)
+        feeHashLabel.setMinimumWidth(420)
+        feeHashLabel.setReadOnly(True)
+        body.addRow(QLabel("<b>FeeHash: </b>"), feeHashLabel)
         body.addRow(QLabel("<b>BlockStart: </b>"), QLabel(str(PropDetailsDlg.data.BlockStart)))
         body.addRow(QLabel("<b>BlockEnd: </b>"), QLabel(str(PropDetailsDlg.data.BlockEnd)))
         body.addRow(QLabel("<b>TotalPayCount: </b>"), QLabel(str(PropDetailsDlg.data.TotalPayCount)))
         body.addRow(QLabel("<b>RemainingPayCount: </b>"), QLabel(str(PropDetailsDlg.data.RemainingPayCount)))
-        body.addRow(QLabel("<b>PaymentAddress: </b>"), QLabel(PropDetailsDlg.data.PaymentAddress))
+        addyLabel = QLineEdit(PropDetailsDlg.data.PaymentAddress)
+        addyLabel.setMinimumWidth(420)
+        addyLabel.setReadOnly(True)
+        body.addRow(QLabel("<b>PaymentAddress: </b>"), addyLabel)
         votes = "<span style='color: green'>%d YEAS</span> / " % PropDetailsDlg.data.Yeas
         votes += "<span style='color: orange'>%d ABSTAINS</span> / " % PropDetailsDlg.data.Abstains
         votes += "<span style='color: red'>%d NAYS</span>" % PropDetailsDlg.data.Nays
