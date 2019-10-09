71678f62b8075ce2e8ea271d77bc2d8c7b77be09
random-zebra <random.zebra@protonmail.com>
2019-10-09 22:04:20 +0200

    [TEMP] new sigs test working

--- src/masternode.py
+++ src/masternode.py
@@ -8,9 +8,10 @@ import time
 from PyQt5.Qt import QObject
 from PyQt5.QtCore import pyqtSignal
 
+from constants import NewSigsActive
 from misc import printOK, printDbg, printException, getCallerName, getFunctionName, ipport
 from pivx_hashlib import wif_to_privkey
-from utils import ecdsa_sign, num_to_varint, ipmap, serialize_input_str
+from utils import ecdsa_sign, ecdsa_sign_bin, num_to_varint, ipmap, serialize_input_str
 
 
 class Masternode(QObject):
@@ -35,21 +36,44 @@ class Masternode(QObject):
         self.nodePath = "%d'/0/%d" % (self.hwAcc, self.spath)
         self.collateral = collateral
         self.isTestnet = isTestnet
+        self.currHeight = 0
         Masternode.mnCount += 1
         printOK("Initializing MNode with collateral: %s" % self.nodePath)
 
 
-
-    def signature1(self, device):
+    def getOldBroadcastMessage(self):
         self.sig_time = int(time.time())
         serializedData = ipport(self.ip, self.port)
         serializedData += str(self.sig_time)
         serializedData += binascii.unhexlify(bitcoin.hash160(bytes.fromhex(self.collateral['pubKey'])))[::-1].hex()
         serializedData += binascii.unhexlify(bitcoin.hash160(bytes.fromhex(self.mnPubKey)))[::-1].hex()
         serializedData += str(self.protocol_version)
-        printDbg("Masternode PubKey: %s" % self.mnPubKey)
-        printDbg("SerializedData: MY_IP:%s" % serializedData.split(':')[1])
+        return serializedData
+
+
+    def getNewBroadcastMessage(self):
+        self.sig_time = int(time.time())
+        pk1 = bytes.fromhex(self.collateral['pubKey'])
+        pk2 = bytes.fromhex(self.mnPubKey)
+        ss = (1).to_bytes(4, byteorder='little')
+        ss += bytes.fromhex(ipmap(self.ip, self.port))
+        ss += (self.sig_time).to_bytes(8, byteorder='little')
+        ss += (len(pk1).to_bytes(1, byteorder='little') + pk1)
+        ss += (len(pk2).to_bytes(1, byteorder='little') + pk2)
+        ss += (self.protocol_version).to_bytes(4, byteorder='little')
+        res = bitcoin.bin_dbl_sha256(ss)[::-1]
+        return res.hex()
+
+
+    def signature1(self, device):
         try:
+            fNewSigs = NewSigsActive(self.currHeight, self.isTestnet)
+            if fNewSigs:
+                serializedData = self.getNewBroadcastMessage()
+            else:
+                serializedData = self.getOldBroadcastMessage()
+            printDbg("SerializedData: %s" % serializedData)
+            # HW wallet signature
             device.signMess(self.tab_main.caller, self.nodePath, serializedData, self.isTestnet)
             #wait for signal when device.sig1 is ready then --> finalizeStartMessage
         except Exception as e:
@@ -61,12 +85,38 @@ class Masternode(QObject):
         return None
 
 
+    def getOldPingMessage(self, block_hash):
+        scriptSig = ''
+        sequence = 0xffffffff
+        return serialize_input_str(self.collateral['txid'],
+                                   self.collateral['txidn'],
+                                   sequence, scriptSig) + \
+               block_hash + str(self.sig_time)
 
-    def signature2(self, serializedData):
+
+    def getNewPingMessage(self, block_hash):
+        ss = bytes.fromhex(self.collateral["txid"])[::-1]
+        ss += (self.collateral["txidn"]).to_bytes(4, byteorder='little')
+        ss += bytes([0, 255, 255, 255, 255])
+        ss += bytes.fromhex(block_hash)[::-1]
+        ss += (self.sig_time).to_bytes(8, byteorder='little')
+        return bitcoin.bin_dbl_sha256(ss)
+
+
+    def signature2(self, block_hash):
         try:
-            # local
-            sig2 = ecdsa_sign(serializedData, self.mnWIF)
-            return (b64decode(sig2).hex())
+            fNewSigs = NewSigsActive(self.currHeight, self.isTestnet)
+            if fNewSigs:
+                mnping = self.getNewPingMessage(block_hash)
+                printDbg("mnping: %s" % mnping.hex())
+                sig2 = ecdsa_sign_bin(mnping, self.mnWIF) # local
+            else:
+                mnping = self.getOldPingMessage(block_hash)
+                printDbg("mnping: %s" % mnping)
+                sig2 = ecdsa_sign(mnping, self.mnWIF)
+
+            return (b64decode(sig2).hex()), fNewSigs
+
         except Exception as e:
             err_msg = "error in signature2"
             printException(getCallerName(), getFunctionName(), err_msg, e.args)
@@ -86,25 +136,18 @@ class Masternode(QObject):
         sequence = 0xffffffff
 
         try:
-            # block_hash = hash(currBlock-12)
-            currBlock = self.rpcClient.getBlockCount()
-            if currBlock is None:
-                raise Exception('Unable to get current block number')
-            block_hash = self.rpcClient.getBlockHash(currBlock - 12)
+            block_hash = self.rpcClient.getBlockHash(self.currHeight - 12)
             if block_hash is None:
-                raise Exception('Unable to get blockhash for block %d' % currBlock - 12)
+                raise Exception('Unable to get blockhash for block %d' % self.currHeight - 12)
 
-            printDbg("Current block from PIVX client: %s" % str(currBlock))
+            printDbg("Current block from PIVX client: %s" % str(self.currHeight))
             printDbg("Hash of 12 blocks ago: %s" % block_hash)
 
             vintx = bytes.fromhex(self.collateral['txid'])[::-1].hex()
             vinno = self.collateral['txidn'].to_bytes(4, byteorder='big')[::-1].hex()
             vinsig = num_to_varint(len(scriptSig) / 2).hex() + bytes.fromhex(scriptSig)[::-1].hex()
             vinseq = sequence.to_bytes(4, byteorder='big')[::-1].hex()
-
             ipv6map = ipmap(self.ip, self.port)
-            #printDbg("ipv6map: %s" % ipv6map)
-
             collateral_in = num_to_varint(len(self.collateral['pubKey'])/2).hex() + self.collateral['pubKey']
             delegate_in = num_to_varint(len(self.mnPubKey)/2).hex() + self.mnPubKey
 
@@ -116,10 +159,8 @@ class Masternode(QObject):
         work_sig_time = self.sig_time.to_bytes(8, byteorder='big')[::-1].hex()
         work_protoversion = self.protocol_version.to_bytes(4, byteorder='big')[::-1].hex()
         last_ping_block_hash = bytes.fromhex(block_hash)[::-1].hex()
-        serializedData = serialize_input_str(self.collateral['txid'], self.collateral['txidn'], sequence, scriptSig) + block_hash + str(self.sig_time)
 
-        printDbg("serializedData: %s" % serializedData)
-        sig2 = self.signature2(serializedData)
+        sig2, fNewSigs = self.signature2(block_hash)
         printOK("second signature: %s" % sig2)
 
         work = vintx + vinno + vinsig + vinseq
@@ -129,9 +170,15 @@ class Masternode(QObject):
         work += vintx + vinno + vinsig + vinseq
         work += last_ping_block_hash + work_sig_time
         work += num_to_varint(len(sig2) / 2).hex() + sig2
-        # nnLastDsq to zero
-        work += "0"*16
+        if fNewSigs:
+            work += (1).to_bytes(4, byteorder='little').hex()
+            work += (1).to_bytes(4, byteorder='little').hex()
+        else:
+            # nnLastDsq to zero
+            work += "0" * 16
+
         # Emit signal
+        printDbg("EMITTING: %s" % work)
         self.sigdone.emit(work)
 
 
@@ -139,8 +186,16 @@ class Masternode(QObject):
     def startMessage(self, device, rpcClient):
         # setuo rpc connection
         self.rpcClient = rpcClient
-        # update protocol version
-        self.protocol_version = self.rpcClient.getProtocolVersion()
+        try:
+            # update protocol version
+            self.protocol_version = self.rpcClient.getProtocolVersion()
+
+            # set current height
+            self.currHeight = self.rpcClient.getBlockCount()
+        except Exception as e:
+            err_msg = "error in startMessage"
+            printException(getCallerName(), getFunctionName(), err_msg, e)
+            return
         # done signal from hwdevice thread
         device.sig1done.connect(self.finalizeStartMessage)
         # prepare sig1 (the one done on the hw device)
